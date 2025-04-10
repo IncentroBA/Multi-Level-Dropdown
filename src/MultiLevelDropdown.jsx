26ec9810355d5e6d2d06fedf0fa0d226385eb721
Rob Duits <rob.duits@incentro.com>
2025-04-10 13:26:55 +0200

    Add setting to display submenu item in output

--- src/MultiLevelDropdown.jsx
+++ src/MultiLevelDropdown.jsx
@@ -1,13 +1,14 @@
 import "./ui/MultiLevelDropdown.css";
 import { createElement, useEffect, useState, useCallback, useMemo } from "react";
 
-export function MultiLevelDropdown({ DataJSON, placeholder, selectedOption }) {
+export function MultiLevelDropdown({ DataJSON, displaySubmenuString, placeholder, selectedOption }) {
     const [canRender, setCanRender] = useState(false);
     const [currentMenu, setCurrentMenu] = useState("main");
     const [isOpen, setIsOpen] = useState(false);
     const [menuData, setMenuData] = useState(null);
     const [menuStack, setMenuStack] = useState([]);
     const [selectedValue, setSelectedValue] = useState();
+    const [submenuPath, setSubmenuPath] = useState("");
 
     const currentItems = useMemo(() => {
         if (!menuData || !currentMenu) {
@@ -53,17 +54,19 @@ export function MultiLevelDropdown({ DataJSON, placeholder, selectedOption }) {
     }, [DataJSON]);
 
     if (!canRender || !menuData) {
-        return <div className="multilevel-dropdown form-group"></div>;
+        return <div className="multilevel-dropdown"></div>;
     }
 
     return (
-        <div
-            className="multilevel-dropdown form-group"
-            onMouseEnter={() => setIsOpen(true)}
-            onMouseLeave={() => setIsOpen(false)}
-        >
+        <div className="multilevel-dropdown" onMouseEnter={() => setIsOpen(true)} onMouseLeave={() => setIsOpen(false)}>
             <div className="form-control">
-                <span>{selectedValue || placeholder}</span>
+                <span>
+                    {selectedValue
+                        ? displaySubmenuString && submenuPath
+                            ? `${submenuPath} > ${selectedValue}`
+                            : selectedValue
+                        : placeholder}
+                </span>
                 <div className="multilevel-dropdown__icon">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <path d="M8.12496 9.00002L12.005 12.88L15.885 9.00002C16.275 8.61002 16.905 8.61002 17.295 9.00002C17.685 9.39002 17.685 10.02 17.295 10.41L12.705 15C12.315 15.39 11.685 15.39 11.295 15L6.70496 10.41C6.51771 10.2232 6.41248 9.96954 6.41248 9.70502C6.41248 9.4405 6.51771 9.18685 6.70496 9.00002C7.09496 8.62002 7.73496 8.61002 8.12496 9.00002Z" />
@@ -91,6 +94,8 @@ export function MultiLevelDropdown({ DataJSON, placeholder, selectedOption }) {
                                     if (menuData[item.value]) {
                                         navigateToSubmenu(item.value, item.label);
                                     } else {
+                                        const currentPath = menuStack.map(item => item.label).join(" > ");
+                                        setSubmenuPath(currentPath);
                                         setSelectedValue(item.label);
                                         selectedOption.setValue(item.label);
                                         setIsOpen(false);
